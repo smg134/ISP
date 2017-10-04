49aa670b7680bd6f757067cc7c60b3cfb5f8368d
Sean Gaffney <smg134@zips.uakron.edu>
2017-10-03 21:17:02 -0400

    Added a lot of div's and js function to display them

---
+++ assignment3/javascript.js
@@ -0,0 +1,87 @@
+function displayTitle() {
+	document.getElementById("title").style.display = "block";
+	document.getElementById("header").style.display = "none";
+	document.getElementById("subheader").style.display = "none";
+	document.getElementById("paragraph").style.display = "none";
+	document.getElementById("bg-color").style.display = "none";
+	document.getElementById("bg-image").style.display = "none";
+	document.getElementById("image").style.display = "none";
+	document.getElementById("animation").style.display = "none";
+}
+
+function displayHeader() {
+	document.getElementById("title").style.display = "none";
+	document.getElementById("header").style.display = "block";
+	document.getElementById("subheader").style.display = "none";
+	document.getElementById("paragraph").style.display = "none";
+	document.getElementById("bg-color").style.display = "none";
+	document.getElementById("bg-image").style.display = "none";
+	document.getElementById("image").style.display = "none";
+	document.getElementById("animation").style.display = "none";
+}
+
+function displaySubheader() {
+	document.getElementById("title").style.display = "none";
+	document.getElementById("header").style.display = "none";
+	document.getElementById("subheader").style.display = "block";
+	document.getElementById("paragraph").style.display = "none";
+	document.getElementById("bg-color").style.display = "none";
+	document.getElementById("bg-image").style.display = "none";
+	document.getElementById("image").style.display = "none";
+	document.getElementById("animation").style.display = "none";
+}
+
+function displayParagraph() {
+	document.getElementById("title").style.display = "none";
+	document.getElementById("header").style.display = "none";
+	document.getElementById("subheader").style.display = "none";
+	document.getElementById("paragraph").style.display = "block";
+	document.getElementById("bg-color").style.display = "none";
+	document.getElementById("bg-image").style.display = "none";
+	document.getElementById("image").style.display = "none";
+	document.getElementById("animation").style.display = "none";
+}
+
+function displayBackgroundColor() {
+	document.getElementById("title").style.display = "none";
+	document.getElementById("header").style.display = "none";
+	document.getElementById("subheader").style.display = "none";
+	document.getElementById("paragraph").style.display = "none";
+	document.getElementById("bg-color").style.display = "block";
+	document.getElementById("bg-image").style.display = "none";
+	document.getElementById("image").style.display = "none";
+	document.getElementById("animation").style.display = "none";
+}
+
+function displayBackgroundImage() {
+	document.getElementById("title").style.display = "none";
+	document.getElementById("header").style.display = "none";
+	document.getElementById("subheader").style.display = "none";
+	document.getElementById("paragraph").style.display = "none";
+	document.getElementById("bg-color").style.display = "none";
+	document.getElementById("bg-image").style.display = "block";
+	document.getElementById("image").style.display = "none";
+	document.getElementById("animation").style.display = "none";
+}
+
+function displayImage() {
+	document.getElementById("title").style.display = "none";
+	document.getElementById("header").style.display = "none";
+	document.getElementById("subheader").style.display = "none";
+	document.getElementById("paragraph").style.display = "none";
+	document.getElementById("bg-color").style.display = "none";
+	document.getElementById("bg-image").style.display = "none";
+	document.getElementById("image").style.display = "block";
+	document.getElementById("animation").style.display = "none";
+}
+
+function displayAnimation() {
+	document.getElementById("title").style.display = "none";
+	document.getElementById("header").style.display = "none";
+	document.getElementById("subheader").style.display = "none";
+	document.getElementById("paragraph").style.display = "none";
+	document.getElementById("bg-color").style.display = "none";
+	document.getElementById("bg-image").style.display = "none";
+	document.getElementById("image").style.display = "none";
+	document.getElementById("animation").style.display = "block";
+}
