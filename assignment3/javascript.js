a07516b69daa39eec254b392a57d8ebf3adc1f12
Sean Gaffney <smg134@zips.uakron.edu>
2017-10-04 14:57:44 -0400

    Nearly done with javascript functions

--- assignment3/javascript.js
+++ assignment3/javascript.js
@@ -85,3 +85,65 @@ function displayAnimation() {
 	document.getElementById("image").style.display = "none";
 	document.getElementById("animation").style.display = "block";
 }
+
+var page = "<!DOCTYPE html><html>";
+var window;
+
+function setTitle() {
+	page += "<title>" + document.getElementById("title_form")[0].value + "</title>";
+	return false;
+}
+
+function addHeader() {
+	page += "<h1 style=\"color:" + document.getElementById("header_form")[1].value + "\">" + document.getElementById("header_form")[0].value + "</h1>";
+	return false;
+}
+
+function addSubheader() {
+	page +=  "<h2 style=\"color:" + document.getElementById("subheader_form")[1].value + "\">" + document.getElementById("subheader_form")[0].value + "</h2>";
+	return false;
+}
+
+function addParagraph() {
+	page += "<p style=\"color:" + document.getElementById("paragraph_form")[1].value + "\">" + document.getElementById("paragraph_form")[0].value + "</p>";
+	alert(page);
+	return false;
+}
+
+function setBackgroundColor() {
+	page += "<style>body{background-color: " + document.getElementById("bg-color_form")[0].value + ";}</style>";
+	alert(page);
+	return false;
+}
+
+function setBackgroundImage() {
+	//TODO: do this
+}
+
+function addImage() {
+	page += "<img src=\"" + document.getElementsByName("imageInput")[0].value + "\" alt=\"Image not found\" style=\"width:" + document.getElementsByName("imageWidth")[0].value + ";height:" + document.getElementsByName("imageHeight")[0].value + ";\">";
+	return false;
+}
+
+function addAnimation() {
+	//container
+	page += "<style>#container{width: " + document.getElementById("container_form")[0].value + "; height: " + document.getElementById("container_form")[1].value + "; postition: relative; background: " + document.getElementById("container_form")[2].value + "; }";
+	//animate
+	page += "#animate { width: " + document.getElementById("animate_form")[0].value + "; height: " + document.getElementById("animate_form")[1].value + "; position: absolute; background-color: " + document.getElementById("animate_form")[2].value + "; }</style><body><div id = \"container\"><div id = \"animate\"></div></div>";	
+	//style
+	var type = document.getElementById("animation-style_form")[0].value;
+	if (type == 1) {
+		page += "<script>function Move() { var elem = document.getElementById(" + "\"" + "animate" + "\""  + "); var pos = 0; var id = setInterval(frame, 5); function frame() { if (pos == 350) { clearInterval(id); } else { pos++;  elem.style.top = pos + 'px'; } } }";
+		page += "</scr" + "ipt>";
+	}
+	else if (type == 2) {
+		page += "<script>function Move() { var elem = document.getElementById(" + "\"" + "animate" + "\""  + "); var pos = 0; var id = setInterval(frame, 5); function frame() { if (pos == 350) { clearInterval(id); } else { pos++;  elem.style.left = pos + 'px'; } } }";
+		page += "</scr" + "ipt>";
+	}
+	else if (type == 3) {
+		page += "<script>function Move() { var elem = document.getElementById(" + "\"" + "animate" + "\""  + "); var pos = 0; var id = setInterval(frame, 5); function frame() { if (pos == 350) { clearInterval(id); } else { pos++;  elem.style.top = pos + 'px';  elem.style.left = pos + 'px'; } } }";			
+		page += "</scr" + "ipt>";
+	}
+	alert(page);
+	return false;
+}
